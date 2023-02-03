63a4ad26d73a3c7632b5291a71b388f28e0ad892
Ines Alejandro Garcia Mosqueda <inesgamos@gmail.com>
2023-02-02 23:53:40 -0600

    Create practica1.sol

---
+++ solidityPractice/practica1.sol
@@ -0,0 +1,14 @@
+//Buena practica crear la clase con el mismo nombre del archivo
+//Licencia (buena practica)
+//SPDX-License-Identifier: GPL-3.0
+
+//version >= 0.7.0 <9.0.0
+pragma solidity >= 0.7.0 <9.0.0;
+
+//Declaracion de contrato
+
+contract practica1{ //Cualquier nombre
+    constructor(){
+        //codigo de inicializacion
+    }
+}
